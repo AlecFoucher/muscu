641e790010ec6ff63d0d1b75e76158f16d5bc782
Alec Foucher <alec.foucher@viacesi.fr>
2026-05-03 23:35:06 +0200

    changement de split

--- js/data.js
+++ js/data.js
@@ -4,26 +4,29 @@ const PROGRAM = {
     { group: "Pectoraux", name: "Développé incliné haltères", sets: 3, reps: "8-12", rest: 90 },
     { group: "Épaules", name: "Shoulder Press machine", sets: 3, reps: "8-12", rest: 90 },
     { group: "Épaules", name: "Élévations latérales haltères", sets: 4, reps: "12-15", rest: 60 },
-    { group: "Triceps", name: "Triceps Pushdown (poulie)", sets: 3, reps: "10-12", rest: 60 },
-    { group: "Triceps", name: "Dips assistés", sets: 3, reps: "max", rest: 90 },
+    { group: "Triceps", name: "Triceps Pushdown (corde)", sets: 3, reps: "10-12", rest: 60 },
+    { group: "Triceps", name: "Extension triceps au-dessus de la tête (poulie)", sets: 3, reps: "12", rest: 60 },
+    { group: "Cardio", name: "Tapis incliné ou vélo", sets: 1, reps: "10-15 min", rest: 0 },
   ],
 
   Mardi: [
     { group: "Dos", name: "Lat Pulldown (tirage vertical)", sets: 4, reps: "8-12", rest: 90 },
-    { group: "Dos", name: "Seated Row (tirage horizontal)", sets: 4, reps: "8-12", rest: 90 },
+    { group: "Dos", name: "Seated Row machine", sets: 4, reps: "8-12", rest: 90 },
     { group: "Dos", name: "Row haltère unilatéral", sets: 3, reps: "10-12", rest: 75 },
     { group: "Arrière épaules", name: "Face Pull (poulie)", sets: 3, reps: "12-15", rest: 60 },
     { group: "Biceps", name: "Curl barre EZ", sets: 3, reps: "10-12", rest: 60 },
     { group: "Biceps", name: "Curl marteau haltères", sets: 3, reps: "10-12", rest: 60 },
+    { group: "Cardio", name: "Tapis incliné ou vélo", sets: 1, reps: "10-15 min", rest: 0 },
   ],
 
   Mercredi: [
-    { group: "Jambes", name: "Leg Press", sets: 4, reps: "8-12", rest: 120 },
+    { group: "Jambes", name: "Leg Press machine", sets: 4, reps: "8-12", rest: 120 },
     { group: "Quadriceps", name: "Leg Extension", sets: 3, reps: "12", rest: 60 },
     { group: "Ischios", name: "Leg Curl", sets: 3, reps: "12", rest: 60 },
     { group: "Fessiers", name: "Hip Thrust (barre ou machine)", sets: 3, reps: "10-12", rest: 90 },
     { group: "Mollets", name: "Calf Raise machine", sets: 4, reps: "15", rest: 60 },
     { group: "Abdos", name: "Crunch machine", sets: 4, reps: "15", rest: 45 },
+    { group: "Cardio", name: "Tapis incliné ou vélo", sets: 1, reps: "10-15 min", rest: 0 },
   ],
 
   Jeudi: [],
@@ -33,26 +36,29 @@ const PROGRAM = {
     { group: "Pectoraux", name: "Développé incliné haltères", sets: 3, reps: "8-12", rest: 90 },
     { group: "Épaules", name: "Shoulder Press machine", sets: 3, reps: "8-12", rest: 90 },
     { group: "Épaules", name: "Élévations latérales haltères", sets: 4, reps: "12-15", rest: 60 },
-    { group: "Triceps", name: "Triceps Pushdown (poulie)", sets: 3, reps: "10-12", rest: 60 },
-    { group: "Triceps", name: "Dips assistés", sets: 3, reps: "max", rest: 90 },
+    { group: "Triceps", name: "Triceps Pushdown (corde)", sets: 3, reps: "10-12", rest: 60 },
+    { group: "Triceps", name: "Extension triceps au-dessus de la tête (poulie)", sets: 3, reps: "12", rest: 60 },
+    { group: "Cardio", name: "Tapis incliné ou vélo", sets: 1, reps: "10-15 min", rest: 0 },
   ],
 
   Samedi: [
     { group: "Dos", name: "Lat Pulldown (tirage vertical)", sets: 4, reps: "8-12", rest: 90 },
-    { group: "Dos", name: "Seated Row (tirage horizontal)", sets: 4, reps: "8-12", rest: 90 },
+    { group: "Dos", name: "Seated Row machine", sets: 4, reps: "8-12", rest: 90 },
     { group: "Dos", name: "Row haltère unilatéral", sets: 3, reps: "10-12", rest: 75 },
     { group: "Arrière épaules", name: "Face Pull (poulie)", sets: 3, reps: "12-15", rest: 60 },
     { group: "Biceps", name: "Curl barre EZ", sets: 3, reps: "10-12", rest: 60 },
     { group: "Biceps", name: "Curl marteau haltères", sets: 3, reps: "10-12", rest: 60 },
+    { group: "Cardio", name: "Tapis incliné ou vélo", sets: 1, reps: "10-15 min", rest: 0 },
   ],
 
   Dimanche: [
-    { group: "Jambes", name: "Leg Press", sets: 4, reps: "8-12", rest: 120 },
+    { group: "Jambes", name: "Leg Press machine", sets: 4, reps: "8-12", rest: 120 },
     { group: "Quadriceps", name: "Leg Extension", sets: 3, reps: "12", rest: 60 },
     { group: "Ischios", name: "Leg Curl", sets: 3, reps: "12", rest: 60 },
     { group: "Fessiers", name: "Hip Thrust (barre ou machine)", sets: 3, reps: "10-12", rest: 90 },
     { group: "Mollets", name: "Calf Raise machine", sets: 4, reps: "15", rest: 60 },
     { group: "Abdos", name: "Crunch machine", sets: 4, reps: "15", rest: 45 },
+    { group: "Cardio", name: "Tapis incliné ou vélo", sets: 1, reps: "10-15 min", rest: 0 },
   ],
 };
 
